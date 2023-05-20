9cfb48e7d56a6957c0599d8da0faa09d2b78d472
brittlaw <lawheadbritt@gmail.com>
2023-05-20 02:16:16 -0400

    adding in js for live data

---
+++ index.js
@@ -0,0 +1,51 @@
+function updateTime() {
+  // Los Angeles
+  let miamiElement = document.querySelector("#miami");
+  if (miamiElement) {
+    let miamiDateElement = miamiElement.querySelector(".date");
+    let miamiTimeElement = miamiElement.querySelector(".time");
+    let miamiTime = moment().tz("US/Eastern");
+
+    miamiDateElement.innerHTML = miamiTime.format("MMMM	Do YYYY");
+    miamiTimeElement.innerHTML = miamiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  // Paris
+  let parisElement = document.querySelector("#paris");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+
+    parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
