8b0857972ff9beab8f61cef52efea57ef076fba4
brittlaw <lawheadbritt@gmail.com>
2023-05-22 23:05:46 -0400

    adding in a few extra cities

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function updateTime() {
   // Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let lostAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
+    lostAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  // Miami
   let miamiElement = document.querySelector("#miami");
   if (miamiElement) {
     let miamiDateElement = miamiElement.querySelector(".date");
